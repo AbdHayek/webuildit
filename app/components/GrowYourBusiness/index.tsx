a3096c3d2a6bed1d99c883a0056a63ba8dcbbc80
Ahmad Alhussni <ahmadalhussni2018@gmail.com>
2025-06-15 20:28:43 +0400

    remove useState

--- app/components/GrowYourBusiness/index.tsx
+++ app/components/GrowYourBusiness/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion, AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./GrowYourBusiness.module.scss";
 
 const bubbles = [
