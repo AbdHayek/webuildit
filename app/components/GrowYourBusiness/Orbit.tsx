f9c6835954b4291645dda2564e6866595c957933
Ahmad Alhussni <ahmadalhussni2018@gmail.com>
2025-06-15 21:01:30 +0400

    update compoent

---
+++ app/components/GrowYourBusiness/Orbit.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+export default function Orbit({ centerId }: { centerId: string }) {
+    return (
+        <>
+            {/* outSide Line */}
+            <div className="absolute top-[-5%] right-[10%] left-[10%]">
+                <svg
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 1216 1216"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ opacity: 0.5 }}
+                >
+                    <circle
+                        cx="608"
+                        cy="608"
+                        r="607.5"
+                        stroke={
+                            centerId === `how`
+                                ? "#DA59A6"
+                                : centerId === `who`
+                                    ? "#8448F1"
+                                    : "#408BEC"
+                        }
+                    />
+                </svg>
+            </div>
+
+            {/* inSide Line */}
+            <div className="absolute top-[18%] right-[17%] left-[17%]">
+                <svg
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 992 968"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ opacity: 0.3 }}
+                >
+                    <path
+                        d="M496 1C769.404 1 991 217.269 991 484C991 750.731 769.404 967 496 967C222.596 967 1 750.731 1 484C1 217.269 222.596 1 496 1Z"
+                        stroke={
+                            centerId === `how`
+                                ? "#DA59A6"
+                                : centerId === `who`
+                                    ? "#8448F1"
+                                    : "#408BEC"
+                        }
+                        strokeWidth="2"
+                    />
+                    <defs>
+                        <linearGradient
+                            id="paint0_linear_274_1420"
+                            x1="991.075"
+                            y1="374"
+                            x2="-24.9784"
+                            y2="376.38"
+                            gradientUnits="userSpaceOnUse"
+                        >
+                            <stop stopColor="#8A4EF6" stopOpacity="0.5" />
+                            <stop offset="1" stopColor="#AF04FF" stopOpacity="0.2" />
+                        </linearGradient>
+                    </defs>
+                </svg>
+            </div>
+
+        </>
+    )
+}
---
+++ app/components/GrowYourBusiness/SmallBubbles.tsx
@@ -0,0 +1,406 @@
+import React from 'react'
+import { motion } from "framer-motion";
+
+export default function SmallBubbles({ positionsBubble, centerId }: { positionsBubble: any, centerId: string }) {
+    return (
+        <>
+            <motion.div
+                className={`absolute ${positionsBubble.first} z-[999999] transition-all duration-500`}
+            >
+                <svg
+                    width="98"
+                    height="98"
+                    viewBox="0 0 98 98"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle
+                        cx="48.7963"
+                        cy="48.7962"
+                        r="39.8421"
+                        transform="rotate(165 48.7963 48.7962)"
+                        fill={
+                            centerId === `how`
+                                ? "#DA59A6"
+                                : centerId === `who`
+                                    ? "#8448F1"
+                                    : "#408BEC"
+                        }
+                        fillOpacity="0.25"
+                    />
+                    <g filter="url(#filter0_f_274_1429)">
+                        <circle
+                            cx="48.7966"
+                            cy="48.7964"
+                            r="26.5614"
+                            transform="rotate(165 48.7966 48.7964)"
+                            fill={
+                                centerId === `how`
+                                    ? "#DA59A6"
+                                    : centerId === `who`
+                                        ? "#8448F1"
+                                        : "#408BEC"
+                            }
+                            fillOpacity="0.76"
+                        />
+                    </g>
+                    <g
+                        style={{ mixBlendMode: "plus-lighter" }}
+                        filter="url(#filter1_f_274_1429)"
+                    >
+                        <circle
+                            cx="50.1515"
+                            cy="49.579"
+                            r="9.05877"
+                            transform="rotate(165 50.1515 49.579)"
+                            fill="white"
+                            fillOpacity="0.76"
+                        />
+                    </g>
+                    <defs>
+                        <filter
+                            id="filter0_f_274_1429"
+                            x="19.2286"
+                            y="19.2284"
+                            width="59.136"
+                            height="59.136"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="1.5"
+                                result="effect1_foregroundBlur_274_1429"
+                            />
+                        </filter>
+                        <filter
+                            id="filter1_f_274_1429"
+                            x="39.2906"
+                            y="38.7179"
+                            width="21.7219"
+                            height="21.7221"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="0.9"
+                                result="effect1_foregroundBlur_274_1429"
+                            />
+                        </filter>
+                    </defs>
+                </svg>
+            </motion.div>
+            <motion.div
+                className={`absolute ${positionsBubble.second} z-[999999] transition-all duration-500`}
+            >
+                <svg
+                    width="58"
+                    height="58"
+                    viewBox="0 0 98 98"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle
+                        cx="48.7963"
+                        cy="48.7962"
+                        r="39.8421"
+                        transform="rotate(165 48.7963 48.7962)"
+                        fill={
+                            centerId === `how`
+                                ? "#DA59A6"
+                                : centerId === `who`
+                                    ? "#8448F1"
+                                    : "#408BEC"
+                        }
+                        fillOpacity="0.25"
+                    />
+                    <g filter="url(#filter0_f_274_1429)">
+                        <circle
+                            cx="48.7966"
+                            cy="48.7964"
+                            r="26.5614"
+                            transform="rotate(165 48.7966 48.7964)"
+                            fill={
+                                centerId === `how`
+                                    ? "#DA59A6"
+                                    : centerId === `who`
+                                        ? "#8448F1"
+                                        : "#408BEC"
+                            }
+                            fillOpacity="0.76"
+                        />
+                    </g>
+                    <g
+                        style={{ mixBlendMode: "plus-lighter" }}
+                        filter="url(#filter1_f_274_1429)"
+                    >
+                        <circle
+                            cx="50.1515"
+                            cy="49.579"
+                            r="9.05877"
+                            transform="rotate(165 50.1515 49.579)"
+                            fill="white"
+                            fillOpacity="0.76"
+                        />
+                    </g>
+                    <defs>
+                        <filter
+                            id="filter0_f_274_1429"
+                            x="19.2286"
+                            y="19.2284"
+                            width="59.136"
+                            height="59.136"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="1.5"
+                                result="effect1_foregroundBlur_274_1429"
+                            />
+                        </filter>
+                        <filter
+                            id="filter1_f_274_1429"
+                            x="39.2906"
+                            y="38.7179"
+                            width="21.7219"
+                            height="21.7221"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="0.9"
+                                result="effect1_foregroundBlur_274_1429"
+                            />
+                        </filter>
+                    </defs>
+                </svg>
+            </motion.div>
+            <motion.div
+                className={`absolute ${positionsBubble.third} z-[999999] transition-all duration-500`}
+            >
+                <svg
+                    width="98"
+                    height="98"
+                    viewBox="0 0 98 98"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle
+                        cx="48.7963"
+                        cy="48.7962"
+                        r="39.8421"
+                        transform="rotate(165 48.7963 48.7962)"
+                        fill={
+                            centerId === `how`
+                                ? "#DA59A6"
+                                : centerId === `who`
+                                    ? "#8448F1"
+                                    : "#408BEC"
+                        }
+                        fillOpacity="0.25"
+                    />
+                    <g filter="url(#filter0_f_274_1429)">
+                        <circle
+                            cx="48.7966"
+                            cy="48.7964"
+                            r="26.5614"
+                            transform="rotate(165 48.7966 48.7964)"
+                            fill={
+                                centerId === `how`
+                                    ? "#DA59A6"
+                                    : centerId === `who`
+                                        ? "#8448F1"
+                                        : "#408BEC"
+                            }
+                            fillOpacity="0.76"
+                        />
+                    </g>
+                    <g
+                        style={{ mixBlendMode: "plus-lighter" }}
+                        filter="url(#filter1_f_274_1429)"
+                    >
+                        <circle
+                            cx="50.1515"
+                            cy="49.579"
+                            r="9.05877"
+                            transform="rotate(165 50.1515 49.579)"
+                            fill="white"
+                            fillOpacity="0.76"
+                        />
+                    </g>
+                    <defs>
+                        <filter
+                            id="filter0_f_274_1429"
+                            x="19.2286"
+                            y="19.2284"
+                            width="59.136"
+                            height="59.136"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="1.5"
+                                result="effect1_foregroundBlur_274_1429"
+                            />
+                        </filter>
+                        <filter
+                            id="filter1_f_274_1429"
+                            x="39.2906"
+                            y="38.7179"
+                            width="21.7219"
+                            height="21.7221"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="0.9"
+                                result="effect1_foregroundBlur_274_1429"
+                            />
+                        </filter>
+                    </defs>
+                </svg>
+            </motion.div>
+            <motion.div
+                className={`absolute ${positionsBubble.forth} z-[999999] transition-all duration-500`}
+            >
+                <svg
+                    width="58"
+                    height="58"
+                    viewBox="0 0 98 98"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle
+                        cx="48.7963"
+                        cy="48.7962"
+                        r="39.8421"
+                        transform="rotate(165 48.7963 48.7962)"
+                        fill={
+                            centerId === `how`
+                                ? "#DA59A6"
+                                : centerId === `who`
+                                    ? "#8448F1"
+                                    : "#408BEC"
+                        }
+                        fillOpacity="0.25"
+                    />
+                    <g filter="url(#filter0_f_274_1429)">
+                        <circle
+                            cx="48.7966"
+                            cy="48.7964"
+                            r="26.5614"
+                            transform="rotate(165 48.7966 48.7964)"
+                            fill={
+                                centerId === `how`
+                                    ? "#DA59A6"
+                                    : centerId === `who`
+                                        ? "#8448F1"
+                                        : "#408BEC"
+                            }
+                            fillOpacity="0.76"
+                        />
+                    </g>
+                    <g
+                        style={{ mixBlendMode: "plus-lighter" }}
+                        filter="url(#filter1_f_274_1429)"
+                    >
+                        <circle
+                            cx="50.1515"
+                            cy="49.579"
+                            r="9.05877"
+                            transform="rotate(165 50.1515 49.579)"
+                            fill="white"
+                            fillOpacity="0.76"
+                        />
+                    </g>
+                    <defs>
+                        <filter
+                            id="filter0_f_274_1429"
+                            x="19.2286"
+                            y="19.2284"
+                            width="59.136"
+                            height="59.136"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="1.5"
+                                result="effect1_foregroundBlur_274_1429"
+                            />
+                        </filter>
+                        <filter
+                            id="filter1_f_274_1429"
+                            x="39.2906"
+                            y="38.7179"
+                            width="21.7219"
+                            height="21.7221"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="0.9"
+                                result="effect1_foregroundBlur_274_1429"
+                            />
+                        </filter>
+                    </defs>
+                </svg>
+            </motion.div>
+            {/* end small bubbles */}
+        </>
+    )
+}
