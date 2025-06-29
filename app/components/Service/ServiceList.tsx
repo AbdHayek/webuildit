7d1a86a54db9d4c5d2f771c165371a252b8d92e7
Ahmad Alhussni <ahmadalhussni2018@gmail.com>
2025-06-29 21:22:25 +0400

    update service list section

--- app/components/Service/ServiceList.tsx
+++ app/components/Service/ServiceList.tsx
@@ -53,46 +53,6 @@ export default function ServiceList() {
     const [hasScrolled, setHasScrolled] = useState(false);
     const isLastInView = useInView(lastSectionRef, { margin: "0px 0px -25% 0px" }); // triggers near bottom
 
-    // useEffect(() => {
-    //     let lastScrollY = window.scrollY;
-
-    //     const handleScroll = () => {
-    //         const currentScrollY = window.scrollY;
-
-    //         if (currentScrollY > lastScrollY) {
-    //             setScrollDir("down");
-    //         } else if (currentScrollY < lastScrollY) {
-    //             setScrollDir("up");
-    //         }
-
-    //         lastScrollY = currentScrollY;
-    //     };
-
-    //     window.addEventListener("scroll", handleScroll);
-
-    //     return () => window.removeEventListener("scroll", handleScroll);
-    // }, []);
-
-    // useEffect(() => {
-    //     if (isLastInView && !hasScrolled) {
-    //         if (scrollDir === "down") {
-    //             const percent = 75;
-    //             const pixels = (percent / 100) * window.innerHeight;
-    //             window.scrollBy({ top: pixels, behavior: "smooth" });
-    //             setHasScrolled(true);
-    //         } else if (scrollDir === "up") {
-    //             const percent = 65;
-    //             const pixels = (percent / 100) * window.innerHeight;
-    //             window.scrollBy({ top: -pixels, behavior: "smooth" });
-    //             setHasScrolled(true);
-    //         }
-    //     }
-
-    //     if (!isLastInView) {
-    //         setHasScrolled(false);
-    //     }
-    // }, [isLastInView, scrollDir]);
-
     return (
         <div>
             {servicesData.map((service, index) => {
